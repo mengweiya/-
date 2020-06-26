1219e0f64bab936c93c0f7421c96153e5258931f
liumengwei <lmw9802@163.com>
2020-06-26 16:21:16 +0800

    搭建页面框架+页面适配

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // 实现rem适配
 (function () {
+  // 页面初始化时,,获取当前页面的宽度  约定页面宽度除以80  计算rem的基准值  设置html标签
   var setFont = function () {
+    // 获取文档元素html标签
     var html = document.documentElement
     var width = html.clientWidth
     if (width < 1024) width = 1024
@@ -9,6 +11,7 @@
     html.style.fontSize = fontSize
   }
   setFont()
+  // 在页面尺寸改变的时候调用
   window.onresize = function () {
     setFont()
   }
